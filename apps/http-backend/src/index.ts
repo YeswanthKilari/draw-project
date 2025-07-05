6e599328fcba307a885525fb6e2724cc5b2c8462
yeswanth-7 <yeswanthkilari64@gmail.com>
2025-07-05 19:19:57 +0530

    errors

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,11 +3,13 @@ import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { createroomschema, userschema, signinschema } from "@repo/common/types";
+import { prismaClient} from "@repo/db/client";
 
 
 const app = express();
 const port: number = 3001;
 app.use(express.json());
+
 app.post("/signin", (req: Request, res: Response):any => {
     const data = userschema.safeParse(req.body);
     if (!data.success) {
@@ -26,19 +28,35 @@ app.post("/signin", (req: Request, res: Response):any => {
         token,
     })
 });
-
-app.post("/signup", (req: Request, res: Response):any => {
-    const data = signinschema.safeParse(req.body);
+//@ts-ignore
+app.post("/signup",async (req: Request, res: Response) => {
+    const data = userschema.safeParse(req.body);
     if (!data.success) {
         return res.status(400).json({
             message: "Invalid data",
             errors: data.error.errors,
         });
     }
-    res.json({
-        message: "User signed up successfully",
-        userId: "user123",
+    
+  try {
+    await prismaClient.user.create({
+      data: {
+        email: data.data.username,
+        password: data.data.password,
+        name: data.data.name,
+      },
     });
+      res.json({
+          message: "User signed up successfully",
+          userId: "user123",
+      });
+  } catch (e:any) {
+      console.error("Error creating user:", e);
+      res.status(500).json({
+          message: "Internal server error",
+          error: e.message,
+      });
+  }
 })
 
 app.post("/room",middleware, (req: Request, res: Response):any => {
